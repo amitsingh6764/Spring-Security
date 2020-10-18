2d7d767e27a04746ac86e55122c7c72d4443a97f
Amit Singh <as2085582@gmail.com>
2020-10-18 22:35:46 +0530

    changes

--- Security-1/src/main/java/com/amit/result/Security1Application.java
+++ Security-1/src/main/java/com/amit/result/Security1Application.java
@@ -2,7 +2,7 @@ package com.amit.result;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//let's
 @SpringBootApplication
 public class Security1Application {
 
